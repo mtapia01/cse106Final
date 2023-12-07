19e2feaba88498695e05c91dfc15720dee706ee9
mtapia01 <michaeltapia80@yahoo.com>
2023-12-06 22:26:38 -0800

    Feat(/): can now upload pics and save filename to db

--- app.py
+++ app.py
@@ -6,11 +6,26 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 import os
 
+UPLOAD_FOLDER = 'uploads'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Update with your database URI
 app.config['SECRET_KEY'] = 'key'  # Update with your secret key
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' 
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png'}
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
+
+
+
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(140), nullable=False)
@@ -18,9 +33,24 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    # used to return a str
     def __repr__(self):
         return f"Post('{self.content}', '{self.date_posted}')"
+
+    def save_picture(self, picture):
+        # Save the picture to the server's static folder
+        picture_path = os.path.join(app.config['UPLOAD_FOLDER'], picture.filename)
+        picture.save(picture_path)
+
+        # Set the image field in the Post model to the filename
+        self.image = picture.filename
+
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 #is_authenticated
 class User(db.Model, UserMixin):
@@ -34,21 +64,60 @@ class User(db.Model, UserMixin):
         self.password_hash = generate_password_hash(password)
     #checks hashed passwords
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
+        return check_password_hash(self.password, password)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
+    
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 with app.app_context():
     db.create_all()
 
-
+# Pages
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/create-post')
+def create_post_form():
+    return render_template('upload.html')
+
+# Functions
+@app.route('/create_post', methods=['POST'])
+@login_required
+def create_post():
+    # Get the file and other form data
+    picture = request.files['picture']
+    content = request.form['content']
+    # Get the logged-in user's ID
+    user_id = current_user.id
+
+    # Check if the file is allowed
+    if picture and allowed_file(picture.filename):
+        # Save the picture to the server's static folder
+        picture_path = os.path.join(app.config['UPLOAD_FOLDER'], picture.filename)
+        picture.save(picture_path)
+
+        # Create a new Post instance and set its attributes
+        new_post = Post(content=content, image=picture.filename, user_id=user_id)
+
+        # Add and commit the new_post to the database
+        db.session.add(new_post)
+        db.session.commit()
+
+        return jsonify({'message': 'Post created successfully'})
+
+    return jsonify({'error': 'Invalid file format'})
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -75,25 +144,107 @@ def signup():
 
     return jsonify({'message': 'Signup successful', 'user': {'username': new_user.username}}), 201
 
+@app.route('/get_current_user', methods=['GET'])
+@login_required
+def get_current_user():
+    return jsonify({'user_id': current_user.id})
+
 @app.route('/login', methods=['POST'])
 def login():
+    # username = request.form.get('username')
+    # password = request.form.get('password')
+
     data = request.get_json()
     username = data['username']
     password = data['password']
 
-    #checking if all info is filled
-    if 'username' not in data or 'password' not in data:
+    if not username or not password:
         return jsonify({'error': 'Username and password are required'}), 400
-    
-    #checking if user is in the table
+
+
     user = User.query.filter_by(username=username).first()
+
     if user and user.check_password(password):
-        login_user(user)
-        # Redirect the user to a new page after successful login
-        return redirect(url_for('dashboard'))   
+        # Login successful
+        # session['user_id'] = user.id
+        login_user(user) 
+        return jsonify({'message': 'Login successful', 'user': {'username': user.username}})
     else:
+        # Invalid username or password
         return jsonify({'error': 'Invalid username or password'}), 401
 
+# @app.route('/login', methods=['POST'])
+# def login():
+#     data = request.get_json()
+#     username = data['username']
+#     password = data['password']
+
+#     #checking if all info is filled
+#     if 'username' not in data or 'password' not in data:
+#         return jsonify({'error': 'Username and password are required'}), 400
+    
+#     #checking if user is in the table
+#     user = User.query.filter_by(username=username).first()
+#     if user and user.check_password(password):
+#         login_user(user)
+#         session['user_id'] = user.id
+#         # Redirect the user to a new page after successful login
+#         return redirect(url_for('dashboard'))   
+#     else:
+#         return jsonify({'error': 'Invalid username or password'}), 401
+    
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user_id', None)
+    return jsonify({'message': 'Logout successful'}), 200
+
+# @app.route('/create_post', methods=['POST'])
+# def create_post():
+#     user_id = session.get('user_id')
+
+#     if user_id:
+#         current_user = User.query.get(user_id)
+#         content = request.form.get('content')
+#         picture = request.files.get('picture')
+
+#         new_post = Post(content=content, user_id=user_id)
+
+#         if picture:
+#             new_post.save_picture(picture)
+
+#         db.session.add(new_post)
+#         db.session.commit()
+
+#         return redirect(url_for('dashboard'))  # Redirect to the dashboard or any other page
+#     else:
+#         return jsonify({'error': 'User not authenticated'}), 401
+    
+
+
+@app.route('/dashboardFeed', methods=['GET'])
+def dashboardFeed():
+    user_id = session.get('user_id')
+
+    if user_id:
+        current_user = User.query.get(user_id)
+        if current_user:
+            followed_user_ids = [user.id for user in current_user.followed]
+            feed_posts = []
+
+            for followed_user_id in followed_user_ids:
+                user = User.query.get(followed_user_id)
+                user_posts = user.posts.all()  # Assuming you have a relationship in your User model
+                feed_posts.extend(user_posts)
+
+            # Convert feed_posts to a format that can be easily serialized to JSON
+            serialized_posts = [
+                {'content': post.content, 'image': post.image, 'date_posted': post.date_posted}
+                for post in feed_posts
+            ]
+
+            return jsonify(serialized_posts)
+
+    return jsonify({'error': 'User not authenticated'}), 401
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,56 +1,151 @@
  // Sample data - replace this with your actual data fetching logic
- const userData = {
-    posts: [
-        {
-            id: 1,
-            content: "Post Content 1",
-            image: "post_image_1.jpg",
-            date_posted: "2023-01-01 12:34:56",
-            comments: [
-                { content: "Comment 1", date_posted: "2023-01-02 08:45:30" },
-                { content: "Comment 2", date_posted: "2023-01-03 14:22:18" }
-            ]
-        },
-        {
-            id: 2,
-            content: "Post Content 2",
-            image: "post_image_2.jpg",
-            date_posted: "2023-01-05 18:12:45",
-            comments: [
-                { content: "Comment 3", date_posted: "2023-01-07 09:20:10" }
-                // Additional comments for post 2
-            ]
-        }
-        // Additional posts
-    ]
-};
+//  const userData = {
+//     posts: [
+//         {
+//             id: 1,
+//             content: "Post Content 1",
+//             image: "post_image_1.jpg",
+//             date_posted: "2023-01-01 12:34:56",
+//             comments: [
+//                 { content: "Comment 1", date_posted: "2023-01-02 08:45:30" },
+//                 { content: "Comment 2", date_posted: "2023-01-03 14:22:18" }
+//             ]
+//         },
+//         {
+//             id: 2,
+//             content: "Post Content 2",
+//             image: "post_image_2.jpg",
+//             date_posted: "2023-01-05 18:12:45",
+//             comments: [
+//                 { content: "Comment 3", date_posted: "2023-01-07 09:20:10" }
+//                 // Additional comments for post 2
+//             ]
+//         }
+//         // Additional posts
+//     ]
+// };
 
 // Function to render posts
-function renderPosts() {
+async function renderPosts() {
+    // console.log("loading")
     const postsContainer = document.getElementById("posts-container");
 
-    userData.posts.forEach(post => {
-        const postElement = document.createElement("div");
-        postElement.classList.add("post");
+    try {
+        const response = await fetch('/dashboardFeed');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
-        postElement.innerHTML = `
-            <p>${post.content}</p>
-            ${post.image ? `<img src="${post.image}" alt="Post Image">` : ''}
-            <small>${post.date_posted}</small>
-            <ul>
-                ${post.comments.map(comment => `<li>${comment.content} - ${comment.date_posted}</li>`).join('')}
-            </ul>
-            <form onsubmit="addComment(${post.id}); return false;">
-                <label for="comment">Add Comment:</label>
-                <input type="text" name="comment" required>
-                <button type="submit">Post Comment</button>
-            </form>
-            <hr>
-        `;
+        const postsData = await response.json();
 
-        postsContainer.appendChild(postElement);
-    });
+        postsData.forEach(post => {
+            const postElement = document.createElement("div");
+            postElement.classList.add("post");
+
+            postElement.innerHTML = `
+                <p>${post.content}</p>
+                ${post.image ? `<img src="${post.image}" alt="Post Image">` : ''}
+                <small>${post.date_posted}</small>
+                <ul>
+                    ${post.comments.map(comment => `<li>${comment.content} - ${comment.date_posted}</li>`).join('')}
+                </ul>
+                <form onsubmit="addComment(${post.id}); return false;">
+                    <label for="comment">Add Comment:</label>
+                    <input type="text" name="comment" required>
+                    <button type="submit">Post Comment</button>
+                </form>
+                <hr>
+            `;
+
+            postsContainer.appendChild(postElement);
+        });
+    } catch (error) {
+        console.error('Error fetching and rendering posts:', error.message);
+    }
 }
+window.onload = renderPosts();
+// function renderPosts() {
+//     const postsContainer = document.getElementById("posts-container");
+
+//     userData.posts.forEach(post => {
+//         const postElement = document.createElement("div");
+//         postElement.classList.add("post");
+
+//         postElement.innerHTML = `
+//             <p>${post.content}</p>
+//             ${post.image ? `<img src="${post.image}" alt="Post Image">` : ''}
+//             <small>${post.date_posted}</small>
+//             <ul>
+//                 ${post.comments.map(comment => `<li>${comment.content} - ${comment.date_posted}</li>`).join('')}
+//             </ul>
+//             <form onsubmit="addComment(${post.id}); return false;">
+//                 <label for="comment">Add Comment:</label>
+//                 <input type="text" name="comment" required>
+//                 <button type="submit">Post Comment</button>
+//             </form>
+//             <hr>
+//         `;
+
+//         postsContainer.appendChild(postElement);
+//     });
+// }
+
+async function uploadPost() {
+    event.preventDefault(); // Prevent the default form submission
+
+    try {
+        // Get the file input and its properties
+        const fileInput = document.querySelector('input[name="picture"]');
+        const file = fileInput.files[0];
+        
+        // Generate a new filename using the user's id and time of day
+        const userId = await getUserId(); // Wait for getUserId to complete
+        const timestamp = Date.now();
+        const newFilename = `${userId}_${timestamp}${getFileExtension(file.name)}`;
+
+        // Update the form data with the new filename
+        const form = document.getElementById('postForm');
+        const formData = new FormData(form);
+        formData.set('picture', file, newFilename);
+
+        // Submit the form
+        const response = await fetch('/create_post', {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await response.json(); // Adjust as needed
+        console.log(data); // Adjust as needed
+    } catch (error) {
+        console.error('Error:', error); // Adjust as needed
+    }
+}
+
+// Helper function to get file extension
+function getFileExtension(filename) {
+    return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+}
+
+// Replace this function with your actual function to get the user's id
+function getUserId() {
+    fetch('/get_current_user')
+        .then(response => response.json())
+        .then(data => {
+            // Check if the response contains the user_id
+            if (data && data.user_id) {
+                return data.user_id;
+            } else {
+                console.error('Error retrieving user ID');
+                return null;
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            return null;
+        });
+
+}
+
 
 // Function to add a comment (dummy function for illustration)
 function addComment(postId) {
@@ -64,15 +159,13 @@ function addComment(postId) {
     // You can implement the actual logic to send the comment to the server here
 }
 
-// Render posts on page load
 renderPosts();
 
 async function signup() {
     // console.log(document.getElementById('signupUsername').value)
     const username = document.getElementById('inputUsername').value;
     const password = document.getElementById('inputPassword').value;
-    console.log(password)
-    // return;
+
     try {
         const response = await fetch('/signup', {
             method: 'POST',
@@ -88,19 +181,19 @@ async function signup() {
 
         const data = await response.json();
         console.log('Signup successful:', data);
-
-        // Optionally, you can redirect the user or perform other actions
+        window.location.href = '/dashboard';
     } catch (error) {
         console.error('Error during signup:', error.message);
-        // Handle errors or display a message to the user
     }
 }
 
-function login(){
+async function login(){
     const username = document.getElementById('inputUsername').value;
     const password = document.getElementById('inputPassword').value;
 
-    // Send a POST request to the server
+    console.log(username);
+    console.log(password);
+
     fetch('/login', {
         method: 'POST',
         headers: {
@@ -116,10 +209,28 @@ function login(){
     })
     .then(data => {
         console.log('Login successful', data);
-        // Optionally, you can redirect the user to a new page or perform other actions
+        window.location.href = '/dashboard';
     })
     .catch(error => {
         console.error('Error during signup:', error.message);
-        // Handle errors or display a message to the user
     });
+}
+
+async function userFeed(){
+    fetch('/dashboardFeed', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById("posts-container").append(data);
+    })
 }
